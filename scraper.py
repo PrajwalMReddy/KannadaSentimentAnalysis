e7bf1f818b527dfee149afe82304887c6de8ed05
PrajwalMReddy <48818107+PrajwalMReddy@users.noreply.github.com>
2023-02-11 21:39:21 +0530

    Set Up A Tweet Scraper

--- scraper.py
+++ scraper.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+import twint
+
+
+def next_account():
+    for account in open("accounts.txt", "r"):
+        print(f"ACCOUNT: {account}")
+        yield account.rstrip()
+
+
+def scrape_users_tweets():
+    all_accounts = next_account()
+
+    while True:
+        try:
+            user = twint.Config()
+            user.Limit = 1
+            user.Username = next(all_accounts)
+
+            twint.run.Search(user)
+
+            print("\n")
+
+        except StopIteration:
+            break
+
+
+scrape_users_tweets()
