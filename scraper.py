6f3f9ce7fab8a8cdcb04aa5a6b2a0fc88497150f
PrajwalMReddy <48818107+PrajwalMReddy@users.noreply.github.com>
2023-02-12 12:46:09 +0530

    Process And Store Tweets

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+import json
+import pathlib
+
 import twint
 
 
@@ -5,6 +8,34 @@ def next_account():
     for account in open("accounts.txt", "r"):
         print(f"ACCOUNT: {account}")
         yield account.rstrip()
+
+
+def store_tweets(tweets_dict_list):
+    for tweet_dict in tweets_dict_list:
+        with open(f"tweets/{tweet_dict['Username']}-{tweet_dict['ID']}.json", "w+") as file:
+            file.write(json.dumps(tweet_dict))
+
+
+def process_tweets(tweets_df):
+    tweet_dict_list = []
+
+    for index, row in tweets_df.iterrows():
+        tweet_dict = {
+            "ID": tweets_df.iat[index, 0],
+            "Username": tweets_df.iat[index, 12],
+            "Name": tweets_df.iat[index, 13],
+            "Date": tweets_df.iat[index, 3],
+            "Tweet": tweets_df.iat[index, 6],
+            "Language": tweets_df.iat[index, 7],
+            "Likes": int(tweets_df.iat[index, 22]),
+            "Replies": int(tweets_df.iat[index, 23]),
+            "Retweets": int(tweets_df.iat[index, 24]),
+        }
+
+        # print(tweet_dict)
+        tweet_dict_list.append(tweet_dict)
+
+    store_tweets(tweet_dict_list)
 
 
 def scrape_users_tweets():
@@ -15,9 +46,12 @@ def scrape_users_tweets():
             user = twint.Config()
             user.Limit = 1
             user.Username = next(all_accounts)
+            user.Pandas = True
 
             twint.run.Search(user)
+            tweets = twint.storage.panda.Tweets_df
 
+            process_tweets(tweets)
             print("\n")
 
         except StopIteration:
